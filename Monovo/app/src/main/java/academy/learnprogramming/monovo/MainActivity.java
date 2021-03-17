c5b5145785199cd0cb20820f4c31c6d97081dbf6
Sam Smith <42390375+smithsam7@users.noreply.github.com>
2021-03-17 15:20:42 -0600

    Add files via upload

---
+++ Monovo/app/src/main/java/academy/learnprogramming/monovo/MainActivity.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming.monovo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
